e0a0599e69c46cee3ca2ac1eb94ef5d897960f7a
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-07-11 15:21:46 +0900

    java

--- jv200/src/main/java/com/varxyz/jv200/mod001/HelloTest.java
+++ jv200/src/main/java/com/varxyz/jv200/mod001/HelloTest.java
@@ -4,6 +4,7 @@ public class HelloTest {
 	public static void main(String[] args) {
 		Hello h = new Hello();
 		System.out.println(h.sayHello());
+		System.out.println("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");
 	}
 }
 
